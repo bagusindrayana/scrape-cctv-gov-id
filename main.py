42f88e7a7dee10f936739c61c982373dcd0d6870
Bagus Indrayana <bagusindrayanaindo@gmail.com>
2024-04-16 10:49:38 +0800

    fastapi

--- main.py
+++ main.py
@@ -1,4 +1,41 @@
 from Providers import Jogja,KabBanjar,Malang,BanjarBaru,Bekasi,KabOku,Tol,Samarinda,Bandung,Bali,Cirebon
+from typing import Union
+from fastapi import FastAPI, Response, status, HTTPException
+
+app = FastAPI()
 
 
-print(Cirebon.getList())
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/cctv/{wilayah}",status_code=200)
+def read_item(wilayah: str, q: Union[str, None] = None,response=Response):
+    results = []
+    if wilayah == "Bali":
+        results = Bali.getList()
+    elif wilayah == "Bandung":
+        results = Bandung.getList()
+    elif wilayah == "BanjarBaru":
+        results = BanjarBaru.getList()
+    elif wilayah == "Bekasi":
+        results = Bekasi.getList()
+    elif wilayah == "Cirebon":
+        results = Cirebon.getList()
+    elif wilayah == "Jogja":
+        results = Jogja.getList()
+    elif wilayah == "KabBanjar":
+        results = KabBanjar.getList()
+    elif wilayah == "KabOku":
+        results = KabOku.getList()
+    elif wilayah == "Malang":
+        results = Malang.getList()
+    elif wilayah == "Samarinda":
+        results = Samarinda.getList()
+    elif wilayah == "Tol":
+        results = Tol.getList()
+    else:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return HTTPException(status_code=404, detail="Not Found")
+    return results
